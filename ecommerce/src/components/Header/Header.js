4614e72f54a7730863fc82968a485da2dced8a3a
hermankraus <95387478+hermankraus@users.noreply.github.com>
2023-05-01 15:17:07 -0300

    organizating header elements

---
+++ ecommerce/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css"
+const Header = ({children}) => {
+  return (
+    <div className="header-container">
+        
+        
+        <h1>STRENGTH STORE</h1>
+
+        <div className="login-shop-container"> {children} </div>
+      
+    </div>
+  )
+}
+
+export default Header
