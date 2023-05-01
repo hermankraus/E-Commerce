229e8e1a4c7c88d53398029d9b53d7fb74bc6683
hermankraus <95387478+hermankraus@users.noreply.github.com>
2023-05-01 20:45:15 -0300

    Style of header + form + api form working

--- ecommerce/src/components/ProductRequest/ProductRequest.js
+++ ecommerce/src/components/ProductRequest/ProductRequest.js
@@ -1,21 +1,35 @@
 import "./ProductRequest.css";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 const ProductRequest = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_jvg8a9r', 'template_gwusf2z', form.current, 'YDO4PPWOkYjVV8mfT')
+      .then((result) => {
+          console.log(result.text);
+          alert("sent")
+      }, (error) => {
+          console.log(error.text);
+          alert("error")
+      });
+  };
+
   return (
-    <div className="form-container">
-                <label for="name">Nombre:</label>
-                <input id="name" name="name" type="text" required></input>
-                <label for="email">Email:</label>
-                <input id="email" name="email" type="email" required></input>
-                <label for="metextarea">Mensaje:</label>
-                <textarea name="metextarea" id="metextarea" cols="30" rows="10" required></textarea> 
-            <p>
-                <button  onclick="limpiarFormulario()" value="Limpiar">Limpiar</button>
-                <input type="submit" id="button"  value="Enviar"  ></input>
-            </p>
-            
-    </div>
-  )
-  }
+    <form className="field" ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
 
 export default ProductRequest
