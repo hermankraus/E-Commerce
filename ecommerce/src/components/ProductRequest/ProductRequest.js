ae9a9ca189dea62799d3ee5ae4371255baa6ec6d
hermankraus <95387478+hermankraus@users.noreply.github.com>
2023-05-04 22:40:18 -0300

    partial email validation

--- ecommerce/src/components/ProductRequest/ProductRequest.js
+++ ecommerce/src/components/ProductRequest/ProductRequest.js
@@ -1,21 +1,40 @@
 import "./ProductRequest.css";
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 
 const ProductRequest = () => {
-  const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+
+  const form = useRef();
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+
+  const emailValidator = (event) => {
+  return setEmail(event.target.value); 
+}
+  
+  const sendEmail = (event) => {
+    event.preventDefault();
 
     emailjs.sendForm('service_jvg8a9r', 'template_gwusf2z', form.current, 'YDO4PPWOkYjVV8mfT')
-      .then((result) => {
-          console.log(result.text);
-          alert("sent")
-      }, (error) => {
-          console.log(error.text);
-          alert("error")
+    .then((result) => {
+   
+    if (regex.test(email) == false){ 
+    setError('correo invalido');
+    }
+
+    else{    
+    setError('correo valido');
+    console.log(result.text);
+      /*alert("sent")*/
+    return true;
+    } 
+    }, (error) => {
+        console.log(error.text);
+          /*alert("error")*/
       });
   };
 
@@ -24,7 +43,8 @@ const ProductRequest = () => {
       <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" value={email} onChange={emailValidator}/>
+      <p> {error} </p>
       <label>Message</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
