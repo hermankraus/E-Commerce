b81709f3fa7b0b56fa45c168415ae8d14e3ea3cc
hermankraus <95387478+hermankraus@users.noreply.github.com>
2023-05-11 18:00:12 -0300

    form validation with API completed

--- ecommerce/src/components/ProductRequest/ProductRequest.js
+++ ecommerce/src/components/ProductRequest/ProductRequest.js
@@ -1,71 +1,134 @@
 import "./ProductRequest.css";
-import React, {useState} from 'react';
-// import emailjs from '@emailjs/browser';
-
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ProductRequest = () => {
+  const form = useRef();
 
   const emailRegexp = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
 
   const [values, setValues] = useState({
-      name: '',
-      email: '',
-      msg: '',
+    name: "",
+    email: "",
+    msg: "",
   });
 
   const [errors, setErrors] = useState({
-    name: '',
-    email: '',
-    msg: '',
-
+    name: "Nombre obligatorio",
+    email: "Email incorrecto",
+    msg: "Mensaje obligatorio",
   });
 
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
+  const formResultMessage = {
+    ok: "Formulario enviado",
+    notOk: "Formulario rechazado",
+  };
+
+  let formOK = false;
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+    console.log("values", values);
+  };
+
+  const emailValidator = () => {
+    emailRegexp.test(values.email) === false || values.email === ""
+      ? setErrors((errors) => ({ ...errors, email: "Email incorrecto" }))
+      : setErrors((errors) => ({ ...errors, email: undefined }));
+  };
+  const nameValidator = () => {
+    values.name === ""
+      ? setErrors((errors) => ({ ...errors, name: "Nombre obligatorio" }))
+      : setErrors((errors) => ({ ...errors, name: undefined }));
+  };
+
+  const msgValidator = () => {
+    values.msg === ""
+      ? setErrors((errors) => ({ ...errors, msg: "Mensaje obligatorio" }))
+      : setErrors((errors) => ({ ...errors, msg: undefined }));
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (
+      errors.email === undefined &&
+      errors.msg === undefined &&
+      errors.msg === undefined
+    ) {
+      formOK = true;
+      console.log("ok", formOK);
+
+      emailjs
+        .sendForm(
+          "service_jvg8a9r",
+          "template_gwusf2z",
+          form.current,
+          "0iEDEZ13pK13800xG"
+        )
+        .then((result) => {
+          if (result.status === Number(200)) {
+            formOK = true;
+          } else {
+            formOK = false;
+            alert("Error de servidor");
+          }
+        });
+    } else {
+      formOK = false;
+      console.log("no ok ", formOK);
     }
+  }
 
-    const handleChange = (event) => {
-    
-      setValues({...values,[event.target.name] : event.target.value})
-      console.log('values',values)
+ /* const formMsg = (formOK) => {
+    if (formOK === true) {
+      return formResultMessage.ok;
+    } else {
+      return formResultMessage.notOk;
     }
-    
-    const emailValidator = () => {
-      emailRegexp.test(values.email) === false ? setErrors((errors) => ({ ...errors, email: 'Email incorrecto' })) : setErrors((errors) => ({ ...errors, email: '' }));
-    }
+  };
+*/
+  console.log("errores", errors);
 
-    const emptyValidator = () => {
-      (values.name === '') ? setErrors((errors) => ({ ...errors, name: 'Nombre obligatorio' })) : setErrors((errors) => ({ ...errors, name: '' }));
-
-      (values.msg === '') ? setErrors((errors) => ({ ...errors, msg: 'Mensaje obligatorio' })) : setErrors((errors) => ({ ...errors, msg: '' }));
-
-
-    }
-    
   return (
-
-    <form className="field"  onSubmit={handleSubmit}>
+    <form ref={form} className="field" onSubmit={handleSubmit}>
       <label htmlFor="name"> Nombre </label>
-      <input id="name" type="text" name="name" value={values.name} onChange={handleChange} onBlur={emptyValidator}/>
-      
-      <p>{errors.name}</p>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        value={values.name}
+        onChange={handleChange}
+        onBlur={nameValidator}
+      />
 
+      <p class="text-danger">{errors.name}</p>
 
       <label htmlFor="email"> Email </label>
-      <input  id="email" type="email" name="email" value={values.email} onChange={handleChange} onBlur={emailValidator} />
+      <input
+        id="email"
+        type="email"
+        name="email"
+        value={values.email}
+        onChange={handleChange}
+        onBlur={emailValidator}
+      />
 
-      <p>{errors.email}</p>
-      
+      <p class="text-danger">{errors.email}</p>
+
       <label htmlFor="message">Consulta</label>
-      <textarea name="msg" onChange={handleChange} onBlur={emptyValidator} value={values.msg}/>
+      <textarea
+        name="msg"
+        onChange={handleChange}
+        onBlur={msgValidator}
+        value={values.msg}
+      />
 
-      <p>{errors.msg}</p>
+      <p class="text-danger">{errors.msg}</p>
 
-      
       <input type="submit" value="Send" />
+
     </form>
   );
 };
 
-export default ProductRequest
+export default ProductRequest;
