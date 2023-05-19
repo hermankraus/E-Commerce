fc853b13c3efef386e264b1409a3e0a992d45c6a
hermankraus <95387478+hermankraus@users.noreply.github.com>
2023-05-19 15:37:35 -0300

    login and shop icons

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-//import "./App.css";
+import "./App.css";
 
 import Banner from "./components/Banner/Banner";
 import Dashboard from "./components/Dashboard/Dashboard";
--- ecommerce/src/components/Shop/Shop.js
+++ ecommerce/src/components/Shop/Shop.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Shop = () => {
-  
-  return <div>Shop</div>;
-
+  return <div></div>;
 };
 
 export default Shop;
