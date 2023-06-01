f18b69a356ed251dafa1ac0387dacfdbcf574158
hermankraus <95387478+hermankraus@users.noreply.github.com>
2023-06-01 15:40:56 -0300

    update

--- ecommerce/src/components/Navbar/Navbar.js
+++ ecommerce/src/components/Navbar/Navbar.js
@@ -40,7 +40,7 @@ const Navbar = () => {
                 </li>
 
                 <li className="nav-item">
-                  <Link className="nav-link" to="/account">
+                  <Link className="nav-link" to="/">
                     <LogIn />
                   </Link>
                 </li>
