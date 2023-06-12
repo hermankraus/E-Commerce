bd7bfe6eaac3334bb7f298aa9cb5eebfab4f0469
Ezequias Bonvissuto <ezequiasbonvissuto@gmail.com>
2023-06-12 06:54:53 -0300

    style cards

--- ecommerce/src/components/Dashboard/Card.js
+++ ecommerce/src/components/Dashboard/Card.js
@@ -1,48 +1,48 @@
 import "./Card.css";
 import React, { useEffect, useState, useContext } from "react";
 import { CartContext } from "../../App";
-import { getProductList} from "../../FirebaseCall";
+import { getProductList } from "../../FirebaseCall";
+import { FaShoppingCart } from "react-icons/fa";
 const Card = () => {
   const [productsList, setProductsList] = useState([]);
   const cartContext = useContext(CartContext);
 
-  useEffect(()=>{
-    getProductList().then((filteredData)=>{
-      setProductsList(filteredData)
-    })
-  
-  
-    
-  },[])
+  useEffect(() => {
+    getProductList().then((filteredData) => {
+      setProductsList(filteredData);
+    });
+  }, []);
 
   const handleAddToCart = (product) => {
     cartContext.addToCart(product);
   };
 
   return (
-    <>
+    <div className="containerCard">
       {productsList.map((product) => (
         <div key={product.id}>
           {product.STATE && (
-            <div className="card-div" >
-              <div>
-                <p>{product.BRAND}</p>
-                <p>{product.PRODUCT}</p>
-                <p>${product.PRICE}</p>
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">{product.BRAND}</h4>
+                <h5 className="card-subtitle mb-2 text-muted">
+                  {product.PRODUCT}
+                </h5>
+                <p className="card-price">${product.PRICE}</p>
               </div>
-              <button onClick={() => handleAddToCart(product)}>
-                Agregar al carrito
+
+              <button
+                onClick={() => handleAddToCart(product)}
+                className="btn btn-primary"
+              >
+                <FaShoppingCart /> Agregar al carrito
               </button>
             </div>
           )}
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
 export default Card;
-
-//{cartItems.map((items, index) => (
-//<p>{items.toString()}</p>
-//))}
